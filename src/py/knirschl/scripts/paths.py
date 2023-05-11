27aaf7d5724a889487ae819f395d8058255a1fa1
knirschl <ucubm@student.kit.edu>
2023-05-11 17:44:02 +0200

    Extract needed stuff from experiments into paths and utils

---
+++ src/py/knirschl/scripts/paths.py
@@ -0,0 +1,47 @@
+# Common variables used in the scripts
+
+import os
+
+def get_parent_path(path):
+  return os.path.abspath(os.path.join(path, os.pardir))
+
+def python():
+  return "python"
+
+def python3():
+  return "python3"
+
+## ---- DIRECTORIES ---- 
+root = get_parent_path(get_parent_path(os.path.realpath(__file__)))
+
+# scripts
+scripts_root = os.path.join(root, "scripts")
+# tools
+tools_root = os.path.join(root, "tools")
+
+# github
+ba_github_root = "/home/fili/Documents/KIT/2023/BA"
+code_github_root = os.path.join(ba_github_root, "code")
+
+# results
+output_root = os.path.join(code_github_root, "output")
+families_datasets_root = os.path.join(output_root, "families")
+
+# ---- EXTERNAL PROGRAMS ----
+programs_root = os.path.join(ba_github_root, "resources", "tools")
+# SimPhy
+simphy_root = os.path.join(programs_root, "SimPhy")
+simphy_exec = os.path.join(simphy_root, "simphy")
+simphy_indelible_wrapper_exec = os.path.join(simphy_root, "INDELIble_wrapper.pl")
+# RAxML-NG
+raxml_root = os.path.join(programs_root, "RAxML-NG")
+raxml_exec = os.path.join(raxml_root, "raxml-ng")
+raxml_mpi_exec = ""
+# GeneRax
+generax_exec = os.path.join(programs_root, "GeneRax", "build", "bin", "generax")
+# FastME
+fastme_exec = os.path.join(programs_root, "FastME", "fastme-2.1.6.4", "binaries", "fastme-2.1.6.2-linux64")
+# MPI
+mpischeduler_exec = ""
+# constants
+mpi_scheduler_heuristic = "--split-scheduler"
---
+++ src/py/knirschl/scripts/utils.py
@@ -0,0 +1,76 @@
+import sys
+import os
+import shutil
+
+def printFlush(msg):
+  print(msg)
+  sys.stdout.flush()
+
+def relative_symlink(src, dest):
+  relative_path = os.path.relpath(src, os.path.dirname(dest))
+  tmp = dest + ".sym"
+  os.symlink(relative_path,  tmp)
+  shutil.move(tmp, dest)
+
+def reset_dir(dir_name):
+  shutil.rmtree(dir_name, True)
+  os.makedirs(dir_name)
+
+def create_result_dir(suffix, additional_args = []):
+  base = os.path.join(results_root, suffix) 
+  for arg in additional_args:
+    base += "_" + arg
+  base += "_"
+  result_dir = ""
+  for i in range(0, 10000):
+    result_dir = base + str(i)
+    if (not os.path.isdir(result_dir)):
+      os.makedirs(result_dir)
+      #open(historic, "a+").write("Results directory: " + result_dir + "\n")
+      #print("Results directory: " + result_dir)
+      return os.path.abspath(result_dir)
+
+def checkAndDelete(arg, arguments):
+  if (arg in arguments):
+    arguments.remove(arg)
+    return True
+  return False
+
+def getAndDelete(arg, arguments, default_value):
+  print ("looking for " + arg + " in " + str(arguments))
+  if (arg in arguments):
+    index = arguments.index(arg)
+    res = arguments[index + 1]
+    del arguments[index + 1]
+    del arguments[index]
+    return res
+  else:
+    return default_value
+
+def getArg(arg, arguments, default_value):
+  print ("looking for " + arg + " in " + str(arguments))
+  if (arg in arguments):
+    index = arguments.index(arg)
+    res = arguments[index + 1]
+    return res
+  else:
+    return default_value
+
+def submit(submit_file_path, command, threads, cluster):
+  if (cluster == "normal"):
+    submit_normal(submit_file_path, command, False)
+  elif (cluster == "normald"):
+    submit_normal(submit_file_path, command, True)
+  elif (cluster == "haswell"):
+    submit_haswell(submit_file_path, command, threads, False)
+  elif (cluster == "haswelld"):
+    submit_haswell(submit_file_path, command, threads, True)
+  elif (cluster == "cascade"):
+    submit_cascade(submit_file_path, command, threads, False)
+  elif (cluster == "cascaded"):
+    submit_cascade(submit_file_path, command, threads, True)
+  elif (cluster == "magny"):
+    submit_magny(submit_file_path, command, threads)
+  else:
+    print("unknown cluster " + cluster)
+    sys.exit(1)
