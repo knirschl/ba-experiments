88248c8f2f67203da1751f5d66bec458b1e553d5
knirschl <ucubm@student.kit.edu>
2023-05-12 13:35:49 +0200

    Setup pipeline with new launch scripts

--- src/py/knirschl/pipeline.py
+++ src/py/knirschl/pipeline.py
@@ -9,7 +9,8 @@ import utils
 import paths
 import fam
 import generate_with_simphy as simphy
-import launch_raxmlng__MY as launch_raxmlng
+#import launch_raxmlng__MY as launch_raxmlng
+import launch_raxml
 import launch_generax
 import launch_fastme
 
@@ -22,6 +23,7 @@ class RunFilter():
         self.fastme = True
         self.ba = True
 
+        self.force_overwrite = True
         self.analyze = False # TODO implement
         #self.debug = False
     
@@ -34,49 +36,44 @@ class RunFilter():
 
     def run_methods(self, datadir, subst_model, cores):
         if (self.generate):
-            if (not os.path.isdir(datadir)):
+            if (not os.path.isdir(datadir) or self.force_overwrite):
+                utils.printFlush("Run simphy...")
                 dataset = os.path.basename(datadir)
-                simphy.generate_dataset(dataset)
-        print("*************************************")
+                simphy.generate_dataset(dataset, cores)
+        print("**************************************************************************")
         print("Run tested gene tree inference tools for dataset " + datadir)
-        print("*************************************")
+        print("**************************************************************************")
         if (len(datadir.split("/")) == 1):
           datadir = fam.get_datadir(datadir) 
         save_stdout = sys.stdout
-        redirected_file = os.path.join(datadir, "runs", "logs_run_all_genes." + subst_model + ".txt")
-        print("Redirected logs to " + redirected_file)
+        #redirected_file = os.path.join(datadir, "runs", "logs_run_all_genes." + subst_model + ".txt")
+        #print("Redirected logs to " + redirected_file)
         sys.stdout.flush()
         # RUN
         if(self.raxml):
             utils.printFlush("Run raxml-ng...")
-            try:
-                # TODO
+            #try:
                 # -- my version --
-                launch_raxmlng.run_raxml_all(datadir, subst_model)
-            except Exception as exc:
-                utils.printFlush("Failed running RAxML-NG\n" + str(exc))
+                #launch_raxmlng.run_raxml_all(datadir, subst_model)
+            launch_raxml.run_raxmlng_on_families(datadir, subst_model, cores)
+            #except Exception as exc:
+             #   utils.printFlush("Failed running RAxML-NG\n" + str(exc))
+
+            # TODO launch_raxmlng like fastme 
         if (self.generax):
             utils.printFlush("Run generax...")
             try:
-                # TODO
-                #run_generax.run_generax_on_families(datadir, subst_model, cores)
-                # -- my version --
                 # create families file
                 generax_families_file = os.path.join(datadir, "families.txt")
-                launch_generax.build_generax_families_file(datadir, "", subst_model, generax_families_file)
-                resultsdir = os.path.join(datadir, "misc")
+                launch_generax.build_generax_families_file(datadir, "random", subst_model, generax_families_file)
+                resultsdir = os.path.join(datadir, "runs", subst_model)
                 launch_generax.run_generax(datadir, None, "SPR", "true", generax_families_file, "false", 1, resultsdir)
             except Exception as exc:
                 utils.printFlush("Failed running GeneRax\n" + str(exc))
         if (self.fastme):
             utils.printFlush("Run fastme...")
             try:
-                # TODO
-                # -- my version --
-                output = "misc/fastme-test.newick"
-                launch_fastme.run_fastme_all(datadir, output)
-                # maybe use benoitmorel/tools/families/run_fastme.py ?
-                run_fastme.run_fastme_on_families(datadir, subst_model, 1, 1)
+                launch_fastme.run_fastme_on_families(datadir, "F84", 1, 1)
             except Exception as exc:
                 utils.printFlush("Failed running FastME\n" + str(exc))
         if (self.ba):
@@ -88,7 +85,7 @@ class RunFilter():
             try:
                 # TODO
                 tmp = 0
-                #fast_rf_cells.analyze(datadir, "all", cores)
+                #rf_distance.compute(...) for all
             except Exception as exc:
                 utils.printFlush("Failed running analyze\n" + str(exc))
 
@@ -97,6 +94,10 @@ root_output = paths.families_datasets_root # output/families/
 simphy_parameters = simphy.SimphyParameters()
 datadir = simphy.get_output_dir(simphy_parameters, root_output)
 run_filter = RunFilter()
+run_filter.force_overwrite = False
+run_filter.raxml = False
+#run_filter.generax = False
+run_filter.fastme = False
 start = time.time()
 try:
     run_filter.run_methods(datadir, "GTR+G", 1)
