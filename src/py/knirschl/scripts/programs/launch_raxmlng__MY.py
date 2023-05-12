297592ab704ae226c64a22cfe3368b8cf4f714d8
knirschl <ucubm@student.kit.edu>
2023-05-12 13:34:49 +0200

    Renamed launch_raxmlng to *__MY

--- src/py/knirschl/scripts/programs/launch_raxmlng__MY.py
+++ src/py/knirschl/scripts/programs/launch_raxmlng__MY.py
@@ -6,6 +6,7 @@ sys.path.insert(0, 'scripts')
 sys.path.insert(0, 'tools/families')
 import paths
 import fam
+import metrics
 
 # raxml-ng --msa output/families/ssim_.../families/family_XXX/alignment.msa --model GTR+G
 def get_raxml_command(alignment, model, with_mpi = False, cores = 1):
@@ -32,14 +33,13 @@ def get_run_specs(command):
 def run_raxml_all(datadir, model, with_mpi = False, cores = 1):
     families_dir = fam.get_families_dir(datadir)
     for family in os.listdir(families_dir):
-        run_raxml(datadir, fam.get_alignment(families_dir, family), model, cores, with_mpi)
+        run_raxml(datadir, fam.get_alignment(families_dir, family), model, with_mpi, cores)
 
 def run_raxml(datadir, alignment, model, with_mpi = False, cores = 1):
     command = get_raxml_command(alignment, model, with_mpi, cores)
     run_name = get_run_specs(command)
     start = time.time()
-    #subprocess.check_call(command)
+    subprocess.check_call(command)
     end = time.time() - start
-    #saved_metrics.save_metrics
-    #metrics.save_metrics(datadir, run_name, end, "runtimes")
+    metrics.save_metrics(datadir, run_name, end, "runtimes")
     
