17699fd113f0bfb40d02cf3e73fd1aefabe1e666
knirschl <ucubm@student.kit.edu>
2023-05-22 14:15:20 +0200

    Implement matadd
    Used no functional methods

--- src/cpp/nj/matrix.h
+++ src/cpp/nj/matrix.h
@@ -30,7 +30,8 @@ matrix_t<T> make_zero_matrix(size_t rows, size_t columns) {
 template<typename T>
 void vecadd(vector_t<T>& left, vector_t<T>& right, vector_t<T>& result) {
     std::transform(left.begin(), left.end(), right.begin(),
-                   std::back_inserter(result),
+                   //std::back_inserter(result), // no overwrite
+                   result.begin(), // overwrite
                    [](T a, T b) { return a + b; });
 }
 
@@ -50,9 +51,42 @@ vector_t<T> vecadd(vector_t<T>& left, vector_t<T>& right) {
     return result;
 }
 
-template<typename R, typename S>
-void matadd(matrix_t<R>& left, matrix_t<S> const& right) {
-    //std::transform(left.begin(), left.end(), right.begin(), std::back_inserter(left), std::bit_and<>());
+ /**
+  * Adds two matrices. Writes the result to the given matrix (param 3).
+  * @tparam T
+  * @param left
+  * @param right
+  * @param result
+  */
+ template<typename T>
+ void matadd(matrix_t<T>& left, matrix_t<T>& right, matrix_t<T>& result) {
+     result.resize(left.size());
+     for (int i{}; i < left.size(); i++) {
+         result[i].resize(left[i].size());
+         for (int j{}; j < left[i].size(); j++) {
+             result[i][j] = left[i][j] + right[i][j];
+         }
+     }
+ }
+
+ /**
+  * Adds two matrices. Writes the result to a new matrix and returns that one.
+  * @tparam T
+  * @param left
+  * @param right
+  * @return
+  */
+template<typename T>
+matrix_t<T> matadd(matrix_t<T>& left, matrix_t<T>& right) {
+    matrix_t<T> result;
+    result.resize(left.size());
+    for (int i{}; i < left.size(); i++) {
+        result[i].resize(left[i].size());
+        for (int j{}; j < left[i].size(); j++) {
+            result[i][j] = left[i][j] + right[i][j];
+        }
+    }
+    return result;
 }
 
 /**
@@ -66,22 +100,18 @@ void matadd(matrix_t<R>& left, matrix_t<S> const& right) {
  */
 template<typename R, typename S>
 void vecscale(vector_t<R>& vec, const S scalar, vector_t<R>& result) {
-    std::transform(vec.begin(), vec.end(), std::back_inserter(result), [scalar](R e) { return e * scalar; });
+    std::transform(vec.begin(), vec.end(),
+                   //std::back_inserter(result), // no overwrite
+                   result.begin(), // overwrite
+                   [scalar](R e) { return e * scalar; });
 }
 
-/**
- * Scales a vector by a scalar. Writes the result to a new vector and returns that one.
- * Type is the same as the given vector.
- * @tparam R
- * @tparam S
- * @param vec
- * @param scalar
- * @return
- */
 template<typename R, typename S>
 vector_t<R> vecscale(vector_t<R>& vec, const S scalar) {
     vector_t<R> result;
-    std::transform(vec.begin(), vec.end(), std::back_inserter(result), [scalar](R e) { return e * scalar; });
+    std::transform(vec.begin(), vec.end(),
+                   std::back_inserter(result),
+                   [scalar](R e) { return e * scalar; });
     return result;
 }
 
@@ -93,11 +123,12 @@ vector_t<R> vecscale(vector_t<R>& vec, const S scalar) {
  * @param scalar
  */
 template<typename R, typename S>
-void matscale(matrix_t<R>& mat, const S scalar) {
-    //std::transform(matrix.begin(), matrix.end(), std::back_inserter(matrix), std::multiplies<>());
-    std::for_each(mat.begin(), mat.end(),
-                   [scalar](vector_t<R>& e) {
-        e = vecscale(e, scalar);
+void matscale(matrix_t<R>& mat, const S scalar, matrix_t<R>& result) {
+    std::transform(mat.begin(), mat.end(),
+                   std::back_inserter(result), // no overwrite
+                   //result.begin(), //overwrite
+                   [scalar](auto row) {
+        return vecscale(row, scalar);
     });
 }
 
