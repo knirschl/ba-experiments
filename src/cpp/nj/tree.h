93c6be15ac277846eaa2bed10083e1e178e80029
knirschl <ucubm@student.kit.edu>
2023-05-22 14:13:58 +0200

    Update tree.h to use template as distance

--- src/cpp/nj/tree.h
+++ src/cpp/nj/tree.h
@@ -8,9 +8,8 @@
 #include <string>
 #include <format>
 #include <memory>
-#include <iostream>
-
-using dist_t = double;
+#include <utility>
+#include "../common_types.h"
 
 struct NTree {
     virtual std::string to_string() = 0;
@@ -19,42 +18,47 @@ struct NTree {
 struct NLeaf : NTree {
     std::string id;
 
+    explicit NLeaf(std::string id) {
+        this->id = std::move(id);
+    }
+
     std::string to_string() override {
         return id;
     }
 };
 
+template<typename T>
 struct NNode : NTree {
     std::shared_ptr<NTree> left{};
     std::shared_ptr<NTree> right{};
-    dist_t branch_length_left;
-    dist_t branch_length_right;
+    T branch_length_left;
+    T branch_length_right;
+
+    NNode(std::shared_ptr<NTree> n_left, T bl_left, std::shared_ptr<NTree> n_right, T bl_right) {
+        left = std::move(n_left);
+        branch_length_left = bl_left;
+        right = std::move(n_right);
+        branch_length_right = bl_right;
+    }
 
     std::string to_string() override {
-        /* C++20
         return std::format("({}:{},{}:{})",
                            left->to_string(),
                            branch_length_left,
                            right->to_string(),
                            branch_length_right);
-        */
-        return "("
-            + left->to_string() + ":" + std::to_string(branch_length_left)
-            + "," + right->to_string() + ":" + std::to_string(branch_length_right)
-            + ")";
     }
 };
 
-std::string tree_to_string(NTree& tree) {
-     // C++20
-    return std::format("({});", tree.to_string());
-
-
-    return "(" + tree.to_string() + ")";
+template<typename T>
+std::shared_ptr<NTree> join(std::shared_ptr<NTree>& n_left, T bl_left,
+                            std::shared_ptr<NTree>& n_right, T bl_right) {
+    NNode<T> parent{n_left, bl_left, n_right, bl_right};
+    return std::make_shared<NNode<T>>(parent);
 }
 
-void print_tree(NTree& tree) {
-    std::cout << tree_to_string(tree) << "\n";
+std::string to_fasta(NTree& tree) {
+    return std::format("({});", tree.to_string());
 }
 
 #endif //BA_TREE_H
