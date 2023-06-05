0ee4102d9dc4c0cad239c92a9ad9f249cc0688ea
knirschl <ucubm@student.kit.edu>
2023-06-05 13:44:07 +0200

    Implement CLI parsing

---
+++ src/cpp/io/parse_cli.h
@@ -0,0 +1,82 @@
+//
+// Created by knirschl on 05.06.23.
+//
+
+#ifndef BA_PARSE_CLI_H
+#define BA_PARSE_CLI_H
+
+#include <argparse/argparse.hpp>
+
+// ./thesis --speciestree "species_tree.matrix.phy" --alignmat "alignment.msa.matrix.phy" -p "output-dir/"
+
+auto build_parser(std::string const& name, std::string const& version) {
+    argparse::ArgumentParser program{name, version};
+
+    program.add_description("");
+    program.add_epilog("");
+
+    program.add_argument("-s", "--speciesmat")
+        .required()
+        .help("specify the species tree distance matrix in PHYLIP format");
+
+    program.add_argument("-a", "--alignmat")
+        .required()
+        .help("specify the alignment distance matrix in PHYLIP format");
+
+    //program.add_argument("-f", "--families")
+    //        .help("specify the families file");
+
+    program.add_argument("-p", "--prefix")
+            .default_value(std::string("/"))
+            .help("specify the output prefix");
+
+    return program;
+}
+
+auto parse_and_get(argparse::ArgumentParser& program, int argc, char *argv[]) {
+    try {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program;
+        exit(1);
+    }
+    std::vector<std::string> res{};
+    res.push_back(program.get("-s"));
+    res.push_back(program.get("-a"));
+    res.push_back(program.get("-p"));
+
+    return res;
+}
+
+auto parse(argparse::ArgumentParser& program, int argc, char *argv[]) {
+    try {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::runtime_error& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program;
+        return 1;
+    }
+    return 0;
+}
+
+template<typename T>
+T get(argparse::ArgumentParser& program, std::string const& opt) {
+    return program.get<T>(opt);
+}
+
+std::string getS(argparse::ArgumentParser& program) {
+    return program.get("-s");
+}
+
+std::string getA(argparse::ArgumentParser& program) {
+    return program.get("-a");
+}
+
+std::string getP(argparse::ArgumentParser& program) {
+    return program.get("-p");
+}
+
+#endif //BA_PARSE_CLI_H
--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -2,9 +2,46 @@
 // Created by knirschl on 18.04.23.
 //
 
-#include "test.h"
+#include "io/parse_cli.h"
+#include "io/parse_phylip.h"
+#include "nj/tree.h"
+#include "nj/matrix.h"
+#include "nj/NJSimple.h"
+#include "io/write_tree.h"
+#include <argparse/argparse.hpp>
 
-int main() {
-    test();
+auto convert(vector_t<std::string> string_ids, vector_t<std::shared_ptr<Tree>>& trees) {
+    std::transform(string_ids.begin(), string_ids.end(), std::back_inserter(trees),
+                   [](auto id) { return std::make_shared<Leaf>(id); });
+}
+
+int main(int argc, char *argv[]) {
+    auto cli_parser = build_parser("thesis", "0.1");
+    parse(cli_parser, argc, argv);
+    //std::cout << getS(cli_parser) << "\n" << getA(cli_parser) << "\n" << getP(cli_parser) << "\n";
+    // read species tree
+    auto species_tree_pair = parse_from_file<double>(getS(cli_parser));
+    auto species_tree_mat = species_tree_pair.first;
+    //vector_t<std::shared_ptr<Tree>> species_tree_start_leafs;
+    //convert(species_tree_pair.second, species_tree_start_leafs);
+    // read alignment
+    auto alignment_pair = parse_from_file<double>(getA(cli_parser));
+    auto alignment_mat = alignment_pair.first;
+    vector_t<std::shared_ptr<Tree>> alignment_start_leafs;
+    convert(alignment_pair.second, alignment_start_leafs);
+
+    // calculate
+    matrix_t<double> distMatrix;
+    for (double scale{}; scale <= 1.0; scale += 0.1) {
+        matscale(species_tree_mat, scale, distMatrix);
+        matadd(alignment_mat, distMatrix, distMatrix);
+        //std::cout << "Scaled Species-Tree-Matrix + Alignment-Matrix =\n" << matstr(distMatrix) << "\n\n";
+        std::shared_ptr <Tree> tree = neighborJoining<>(distMatrix, alignment_start_leafs);
+        std::cout << "Neighbor-joined tree: " << to_newick(*tree) << std::endl;
+        write_newick(*tree, getP(cli_parser) + std::to_string(scale) + "S+G.geneTree.newick");
+    }
+
     return 0;
 }
+
+
