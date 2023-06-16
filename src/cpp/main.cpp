a0828cec5af57630e75857437eba842604848e63
knirschl <ucubm@student.kit.edu>
2023-06-16 09:53:09 +0200

    Testing different scalings for NJ

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -33,7 +33,33 @@ int main(int argc, char *argv[]) {
     convert(alignment_pair.second, alignment_start_leafs);
 
     // calculate
+    matrix_t<double> scaledSpecies;
+    matrix_t<double> scaledAlignm;
     matrix_t<double> distMatrix;
+
+    {
+        double scale_species{-1};
+        double scale_alignm{1};
+        std::cout << "Species-Tree-Matrix before scaling =\n" << matstr(species_tree_mat) << "\n\n";
+        matscale(species_tree_mat, scale_species, scaledSpecies);
+        std::cout << "Scaled Species-Tree-Matrix =\n" << matstr(scaledSpecies) << "\n\n";
+        std::cout << "Alignment-Matrix before scaling =\n" << matstr(alignment_mat) << "\n\n";
+        matscale(alignment_mat, scale_alignm, scaledAlignm);
+        std::cout << "Scaled Alignment-Matrix =\n" << matstr(scaledAlignm) << "\n\n";
+        matadd(scaledAlignm, scaledSpecies, distMatrix);
+        std::cout << "Scaled Species-Tree-Matrix + Alignment-Matrix =\n" << matstr(distMatrix) << "\n\n";
+        std::shared_ptr <Tree> tree = neighborJoining<>(distMatrix, alignment_start_leafs);
+        std::cout << "Neighbor-joined tree: " << to_newick(*tree) << std::endl;
+        // double to string without trailing zeros
+        std::ostringstream oss;
+        oss << std::setprecision(8) << std::noshowpoint << scale_species;
+        std::ostringstream osa;
+        osa << std::setprecision(8) << std::noshowpoint << scale_alignm;
+        write_newick(*tree, getP(cli_parser) + oss.str() + "S+" + osa.str() + "G.geneTree.newick");
+    }
+
+
+    /*
     for (double scale{}; scale <= 1.0; scale += 0.1) {
         matscale(species_tree_mat, scale, distMatrix);
         matadd(alignment_mat, distMatrix, distMatrix);
@@ -45,6 +71,7 @@ int main(int argc, char *argv[]) {
         oss << std::setprecision(8) << std::noshowpoint << scale;
         write_newick(*tree, getP(cli_parser) + oss.str() + "S+G.geneTree.newick");
     }
+
     for (int scale{5}; scale <= 100; scale += 5) {
         matscale(species_tree_mat, scale, distMatrix);
         matadd(alignment_mat, distMatrix, distMatrix);
@@ -53,7 +80,7 @@ int main(int argc, char *argv[]) {
         std::cout << "Neighbor-joined tree: " << to_newick(*tree) << std::endl;
         write_newick(*tree, getP(cli_parser) + std::to_string(scale) + "S+G.geneTree.newick");
     }
-
+    */
     return 0;
 }
 
