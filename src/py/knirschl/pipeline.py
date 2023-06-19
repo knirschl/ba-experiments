f29f0746acefecb0249f29b803e16c5b3ebb5db8
knirschl <ucubm@student.kit.edu>
2023-06-19 17:09:31 +0200

    Implement lower branch threshold

--- src/py/knirschl/pipeline.py
+++ src/py/knirschl/pipeline.py
@@ -14,6 +14,7 @@ import launch_raxml
 import launch_generax
 import launch_fastme
 import launch_ba
+import rescale_bl
 import dist_matrix_converter
 import compare_trees
 import metrics
@@ -87,8 +88,10 @@ class RunFilter():
                 utils.printFlush("Failed running FastME\n" + str(exc))
         if (self.ba):
             utils.printFlush("Run ba...")
-            # TODO
             try:
+                # hacky
+                #rescale_bl.lower_threshold_bl(fam.get_true_species_tree(datadir), fam.get_true_species_tree(datadir), 1434018883.83676)
+                
                 dist_matrix_converter.convert_input(datadir)
                 species_tree = fam.get_true_species_tree_matrix_sorted(datadir)
                 launch_ba.run_ba_on_families(datadir, "experimental", species_tree, cores)
@@ -105,12 +108,12 @@ class RunFilter():
 
 root_output = paths.families_datasets_root # output/families/
 # SET simphy PARAMETERS
-simphy_parameters = simphy.SimphyParameters(tag="DL", loss_rate=1.0, dup_rate=1.0)
+simphy_parameters = simphy.SimphyParameters(tag="DTL", species_taxa=50, bl=0.1, transfer_rate=1.0, seed=14) # tag="DL", loss_rate=1.0, dup_rate=1.0, 
 datadir = simphy.get_output_dir(simphy_parameters, root_output)
 print(datadir)
 # TOGGLE PIPELINE ELEMENTS
 # ====== ! CAREFUL ! ======
-#run_filter = RunFilter() # all enabled
+run_filter = RunFilter() # all enabled
 #run_filter.force_overwrite = True # regenerate old dataset
 #run_filter.ba = False
 run_filter.script_ba() # only ba script
--- src/py/knirschl/tools/trees/rescale_bl.py
+++ src/py/knirschl/tools/trees/rescale_bl.py
@@ -14,6 +14,12 @@ def rescale_bl(input_tree, output_tree, scale):
     node.dist *= scale
   tree.write(outfile=output_tree, format = 5, dist_formatter= "%.15f")
 
+def lower_threshold_bl(input_tree, output_tree, threshold):
+  tree = read_tree(input_tree)
+  for node in tree.traverse("postorder"):
+    node.dist = (node.dist, threshold)[node.dist < threshold]
+  tree.write(outfile=output_tree, format=5, dist_formatter="%.15f")
+
 if (__name__ == "__main__"):
   if (len(sys.argv) != 4):
     print("Syntax: python rescale_bl.py input_tree output_tree scale")
