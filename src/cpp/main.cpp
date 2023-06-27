e966214c2c1fd8144b2dad792956ec3893c3c5ab
knirschl <ucubm@student.kit.edu>
2023-06-27 12:42:44 +0200

    Change aS+G values in main

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -34,72 +34,56 @@ int main(int argc, char *argv[]) {
     convert(alignment_pair.second, alignment_start_leafs);
 
     // calculate
-
     {
-        matrix_t<double> scaleMatrix{};
-        matrix_t<double> sumMatrix{};
-        // 0S+1G
-        std::shared_ptr<Tree> tree = neighborJoining<>(alignment_mat, alignment_start_leafs);
-        write_newick(*tree, getP(cli_parser) + "0S+" + "1G.geneTree.newick");
+        double div{100.0};
+        int step{5};
+        for (int i{}; i < 1 * div; i += step) {
+            if (i == 1) {
+                step = 25;
+            }
+            matrix_t<double> scaleMatrix{};
+            matrix_t<double> sumMatrix{};
+            double scale{i / div};
+            matscale(species_tree_mat, scale, scaleMatrix);
+            matadd(alignment_mat, scaleMatrix, sumMatrix);
+            //std::cout << "Scaled Species-Tree-Matrix + Alignment-Matrix =\n" << matstr(sumMatrix) << "\n\n";
+            std::shared_ptr<Tree> tree = neighborJoining<>(sumMatrix, alignment_start_leafs);
+            std::cout << "Neighbor-joined tree: " << to_newick(*tree) << std::endl;
+            // double to string without trailing zeros
+            std::ostringstream oss;
+            oss << std::setprecision(8) << std::noshowpoint << scale;
+            write_newick(*tree, getP(cli_parser) + oss.str() + "S+G.geneTree.newick");
+        }
+        step = 25;
+        for (int i{int(1 * div)}; i <= 2 * div; i += step) {
+            matrix_t<double> scaleMatrix{};
+            matrix_t<double> sumMatrix{};
+            double scale{i / div};
+            matscale(species_tree_mat, scale, scaleMatrix);
+            matadd(alignment_mat, scaleMatrix, sumMatrix);
+            //std::cout << "Scaled Species-Tree-Matrix + Alignment-Matrix =\n" << matstr(distMatrix) << "\n\n";
+            std::shared_ptr<Tree> tree = neighborJoining<>(sumMatrix, alignment_start_leafs);
+            std::cout << "Neighbor-joined tree: " << to_newick(*tree) << std::endl;
+            // double to string without trailing zeros
+            std::ostringstream oss;
+            oss << std::setprecision(8) << std::noshowpoint << scale;
+            write_newick(*tree, getP(cli_parser) + oss.str() + "S+G.geneTree.newick");
+        }
+        for (int i{int(2.5 * div)}; i <= 10 * div; i *= 2) {
+            matrix_t<double> scaleMatrix{};
+            matrix_t<double> sumMatrix{};
+            double scale{i / div};
+            matscale(species_tree_mat, scale, scaleMatrix);
+            matadd(alignment_mat, scaleMatrix, sumMatrix);
+            //std::cout << "Scaled Species-Tree-Matrix + Alignment-Matrix =\n" << matstr(distMatrix) << "\n\n";
+            std::shared_ptr<Tree> tree = neighborJoining<>(sumMatrix, alignment_start_leafs);
+            std::cout << "Neighbor-joined tree: " << to_newick(*tree) << std::endl;
+            // double to string without trailing zeros
+            std::ostringstream oss;
+            oss << std::setprecision(8) << std::noshowpoint << scale;
+            write_newick(*tree, getP(cli_parser) + oss.str() + "S+G.geneTree.newick");
+        }
     }
-    {
-        matrix_t<double> scaleMatrix{};
-        matrix_t<double> sumMatrix{};
-        // 0.3S+1G
-        matscale(species_tree_mat, 0.3, scaleMatrix);
-        matadd(scaleMatrix, alignment_mat, sumMatrix);
-        std::shared_ptr<Tree> tree = neighborJoining<>(sumMatrix, alignment_start_leafs);
-        write_newick(*tree, getP(cli_parser) + "0.3S+" + "1G.geneTree.newick");
-    }
-    {
-        matrix_t<double> scaleMatrix{};
-        matrix_t<double> sumMatrix{};
-        // 1S+1G
-        matadd(species_tree_mat, alignment_mat, sumMatrix);
-        std::shared_ptr<Tree> tree = neighborJoining<>(sumMatrix, alignment_start_leafs);
-        write_newick(*tree, getP(cli_parser) + "1S+" + "1G.geneTree.newick");
-    }
-    {
-        matrix_t<double> scaleMatrix{};
-        matrix_t<double> sumMatrix{};
-        // 10S+1G
-        matscale(species_tree_mat, 10, scaleMatrix);
-        matadd(scaleMatrix, alignment_mat, sumMatrix);
-        std::shared_ptr<Tree> tree = neighborJoining<>(sumMatrix, alignment_start_leafs);
-        write_newick(*tree, getP(cli_parser) + "10S+" + "1G.geneTree.newick");
-    }
-    {
-        matrix_t<double> scaleMatrix{};
-        matrix_t<double> sumMatrix{};
-        // 100S+1G
-        matscale(species_tree_mat, 100, scaleMatrix);
-        matadd(scaleMatrix, alignment_mat, sumMatrix);
-        std::shared_ptr<Tree> tree = neighborJoining<>(sumMatrix, alignment_start_leafs);
-        write_newick(*tree, getP(cli_parser) + "100S+" + "1G.geneTree.newick");
-    }
-
-    /*
-    for (double scale{}; scale <= 1.0; scale += 0.1) {
-        matscale(species_tree_mat, scale, distMatrix);
-        matadd(alignment_mat, distMatrix, distMatrix);
-        //std::cout << "Scaled Species-Tree-Matrix + Alignment-Matrix =\n" << matstr(distMatrix) << "\n\n";
-        std::shared_ptr <Tree> tree = neighborJoining<>(distMatrix, alignment_start_leafs);
-        std::cout << "Neighbor-joined tree: " << to_newick(*tree) << std::endl;
-        // double to string without trailing zeros
-        std::ostringstream oss;
-        oss << std::setprecision(8) << std::noshowpoint << scale;
-        write_newick(*tree, getP(cli_parser) + oss.str() + "S+G.geneTree.newick");
-    }
-
-    for (int scale{5}; scale <= 100; scale += 5) {
-        matscale(species_tree_mat, scale, distMatrix);
-        matadd(alignment_mat, distMatrix, distMatrix);
-        //std::cout << "Scaled Species-Tree-Matrix + Alignment-Matrix =\n" << matstr(distMatrix) << "\n\n";
-        std::shared_ptr <Tree> tree = neighborJoining<>(distMatrix, alignment_start_leafs);
-        std::cout << "Neighbor-joined tree: " << to_newick(*tree) << std::endl;
-        write_newick(*tree, getP(cli_parser) + std::to_string(scale) + "S+G.geneTree.newick");
-    }
-    */
 
     return 0;
 }
