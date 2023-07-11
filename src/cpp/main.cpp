5bf16b4f2273c4dba37597e32265aad1ba907045
knirschl <ucubm@student.kit.edu>
2023-07-11 12:20:51 +0200

    Extract second nj run into method

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -40,6 +40,26 @@ auto reset(const std::vector<std::string> &species_tree_ids,
     return tree;
 }
 
+void run2(double scale1, double scale2, const std::shared_ptr<Tree> &tree, std::vector<int> &active,
+          const std::vector<std::vector<double>> &species_tree_mat,
+          const std::vector<std::vector<double>> &alignment_mat,
+          argparse::ArgumentParser &cli_parser) {
+    auto speciation_pairs{tree->get_speciation_pairs()};
+    dist_matrix_t specMatrix{alignment_mat};
+    for (auto &pair: speciation_pairs) {
+        // TODO change selected alignment_mat entries
+    }
+    neighborJoining<>(specMatrix, tree, active);
+    std::cout << "Neighbor-joined tree: " << tree->to_newick() << std::endl;
+
+    // double to string without trailing zeros
+    std::ostringstream oss1, oss2;
+    oss1 << std::setprecision(8) << std::noshowpoint << scale1;
+    oss2 << std::setprecision(8) << std::noshowpoint << scale2;
+    write_newick(*tree,
+                 getP(cli_parser) + oss1.str() + "S+G>" + oss2.str() + "S~G.geneTree.newick");
+}
+
 /**
  * Run nj, reroot, nj.
  *
@@ -54,13 +74,16 @@ auto reset(const std::vector<std::string> &species_tree_ids,
  * @param alignment_mat
  * @param cli_parser
  */
-void run(double scale, const std::shared_ptr<Tree> &tree, const std::vector<int> &active,
+void run(double scale1, std::shared_ptr<Tree> &tree, std::vector<int> &active,
          const std::vector<std::vector<double>> &species_tree_mat,
          const std::vector<std::vector<double>> &alignment_mat,
-         argparse::ArgumentParser &cli_parser) {
+         argparse::ArgumentParser &cli_parser,
+         const std::vector<std::basic_string<char>> &species_tree_ids,
+         const std::vector<std::basic_string<char>> &alignment_ids,
+         const std::tuple<std::string, bool, std::string> &map_config) {
     dist_matrix_t scaleMatrix{};
     dist_matrix_t sumMatrix{};
-    matscale(species_tree_mat, scale, scaleMatrix);
+    matscale(species_tree_mat, scale1, scaleMatrix);
     matadd(alignment_mat, scaleMatrix, sumMatrix);
     //std::cout << "Scaled Species-Tree-Matrix + Alignment-Matrix =\n" << matstr(distMatrix) << "\n\n";
     neighborJoining<>(sumMatrix, tree, active);
@@ -68,21 +91,31 @@ void run(double scale, const std::shared_ptr<Tree> &tree, const std::vector<int>
 
     // tag and reroot
     tree->reroot_APro();
-    auto speciation_pairs{tree->get_speciation_pairs()};
-    dist_matrix_t specMatrix{alignment_mat};
-    for (auto &pair: speciation_pairs) {
-        // TODO change selected alignment_mat entries
-
-    }
     // TODO nj again
-    //reset()
-    neighborJoining<>(specMatrix, tree, active);
-    std::cout << "Neighbor-joined and rerooted tree: " << tree->to_newick() << std::endl;
+    double div{100.0};
+    int step{5};
+    for (int i{}; i < 1 * div; i += step) {
+        tree = reset(species_tree_ids, alignment_ids, map_config);
+        active = leaf_indices;
 
-    // double to string without trailing zeros
-    std::ostringstream oss;
-    oss << std::setprecision(8) << std::noshowpoint << scale;
-    write_newick(*tree, getP(cli_parser) + oss.str() + "S+G.geneTree.newick");
+        if (i == 1) {
+            step = 25;
+        }
+        run2(scale1, i / div, tree, active, species_tree_mat, alignment_mat, cli_parser);
+    }
+    step = 25;
+    for (int i{int(1 * div)}; i <= 2 * div; i += step) {
+        tree = reset(species_tree_ids, alignment_ids, map_config);
+        active = leaf_indices;
+
+        run2(scale1, i / div, tree, active, species_tree_mat, alignment_mat, cli_parser);
+    }
+    for (int i{int(2.5 * div)}; i <= 10 * div; i *= 2) {
+        tree = reset(species_tree_ids, alignment_ids, map_config);
+        active = leaf_indices;
+
+        run2(scale1, i / div, tree, active, species_tree_mat, alignment_mat, cli_parser);
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -113,20 +146,23 @@ int main(int argc, char *argv[]) {
             if (i == 1) {
                 step = 25;
             }
-            run(i / div, tree, active, species_tree_mat, alignment_mat, cli_parser);
+            run(i / div, tree, active, species_tree_mat, alignment_mat, cli_parser,
+                species_tree_ids, alignment_ids, map_config);
         }
         step = 25;
         for (int i{int(1 * div)}; i <= 2 * div; i += step) {
             std::shared_ptr<Tree> tree = reset(species_tree_ids, alignment_ids, map_config);
             std::vector<int> active{leaf_indices};
 
-            run(i / div, tree, active, species_tree_mat, alignment_mat, cli_parser);
+            run(i / div, tree, active, species_tree_mat, alignment_mat, cli_parser,
+                species_tree_ids, alignment_ids, map_config);
         }
         for (int i{int(2.5 * div)}; i <= 10 * div; i *= 2) {
             std::shared_ptr<Tree> tree = reset(species_tree_ids, alignment_ids, map_config);
             std::vector<int> active{leaf_indices};
 
-            run(i / div, tree, active, species_tree_mat, alignment_mat, cli_parser);
+            run(i / div, tree, active, species_tree_mat, alignment_mat, cli_parser,
+                species_tree_ids, alignment_ids, map_config);
         }
     }
 
