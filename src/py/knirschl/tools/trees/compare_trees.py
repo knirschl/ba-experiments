17cfd97b7abbbfa50c2ac6140f6bdac9b8a67545
knirschl <ucubm@student.kit.edu>
2023-07-20 01:17:53 +0200

    Fix compare_tree errors

--- src/py/knirschl/tools/trees/compare_trees.py
+++ src/py/knirschl/tools/trees/compare_trees.py
@@ -2,16 +2,19 @@ import os
 import subprocess
 import sys
 from threading import Thread
-
+from functools import reduce
 sys.path.insert(0, 'scripts')
 sys.path.insert(0, 'tools/families')
 import paths
 import fam
 import metrics
 
-def merge_dicos(dicts):
-    return {k: [d[k] for d in dicts] for k in dicts[0].keys()}
-
+def merge_dicts(dicts): # https://stackoverflow.com/questions/10461531/merge-and-sum-of-two-dictionaries (havoc_method)
+    def reducer(accumulator, element):
+        for key, value in element.items():
+            accumulator[key] = accumulator.get(key, 0) + value
+        return accumulator
+    return reduce(reducer, dicts, {})
 
 def make_key(family, tree):
     return family + '\t' + tree 
@@ -28,10 +31,10 @@ def rf_compare(tree1, tree2):
     lines = out.split("\n")
     rf_abs = lines[0].split(" ")[-1]
     rf_rel = lines[1].split(" ")[-1]
-    print(rf_abs, rf_rel)
+    #print(rf_abs, rf_rel)
     return [float(rf_abs), float(rf_rel)]
 
-def ttask(datadir, family, method_avg_abs_dico, method_avg_rel_dico, fam_counter):
+def ttask(datadir, family, avg_abs_dicos, avg_rel_dicos, fam_counter):
     true_tree = fam.get_true_tree(datadir, family)
     for tree in os.listdir(fam.get_gene_tree_dir(datadir, family)):
         abs_path_tree = os.path.join(fam.get_gene_tree_dir(datadir, family), tree)
@@ -41,15 +44,15 @@ def ttask(datadir, family, method_avg_abs_dico, method_avg_rel_dico, fam_counter
         dist_abs, dist_rel = rf_compare(abs_path_tree, true_tree)
         if dist_abs == "abort":
             # distance error
-            print("I've catched an error, maybe look into this?\n", dist_rel, "\n")
+            #print("I've catched an error, maybe look into this?\n", dist_rel, "\n")
             continue
         # method average
-        if not tree in method_avg_abs_dico:
+        if not tree in avg_abs_dicos[fam_counter]:
             # if tree (= method) not in dico, add
-            method_avg_abs_dico[tree] = 0
-            method_avg_rel_dico[tree] = 0
-        method_avg_abs_dico[tree] = (method_avg_abs_dico[tree] * fam_counter + dist_abs) / (fam_counter + 1)
-        method_avg_rel_dico[tree] = (method_avg_rel_dico[tree] * fam_counter + dist_rel) / (fam_counter + 1)
+            avg_abs_dicos[fam_counter][tree] = 0
+            avg_rel_dicos[fam_counter][tree] = 0
+        avg_abs_dicos[fam_counter][tree] = (avg_abs_dicos[fam_counter][tree] * fam_counter + dist_abs) / (fam_counter + 1)
+        avg_rel_dicos[fam_counter][tree] = (avg_rel_dicos[fam_counter][tree] * fam_counter + dist_rel) / (fam_counter + 1)
 
         # SAVING TO FILE
         # save single distance
@@ -58,24 +61,22 @@ def ttask(datadir, family, method_avg_abs_dico, method_avg_rel_dico, fam_counter
         #metrics.save_metrics(datadir, make_key(family, tree), dist_rel, "rf_distance-rel")
 
 def compare_all(datadir):
-    method_avg_abs_dicos = []
-    method_avg_rel_dicos = []
+    rf_avg_abs_dicos = [{} for _ in range(len(fam.get_families_list(datadir)))]
+    rf_avg_rel_dicos = [{} for _ in range(len(fam.get_families_list(datadir)))]
     fam_counter = 0
     threads = []
     for family in fam.get_families_list(datadir):
-        thread_avg_abs_dico = {}
-        thread_avg_rel_dico = {}
-        fam_thread = Thread(target=ttask, args=(datadir, family, method_avg_abs_dico, method_avg_rel_dico, fam_counter))
-        method_avg_abs_dicos.append(thread_avg_abs_dico)
-        method_avg_rel_dicos.append(thread_avg_rel_dico)
+        fam_thread = Thread(target=ttask, args=(datadir, family, rf_avg_abs_dicos, rf_avg_rel_dicos, fam_counter))
         fam_counter += 1
         threads.append(fam_thread)
     for t in threads:
         t.start()
     for t in threads:
         t.join()
-    avg_abs_dico = merge_dicos(method_avg_abs_dicos)
-    avg_rel_dico = merge_dicos(method_avg_rel_dicos)
+    #print(rf_avg_abs_dicos)
+    avg_abs_dico = merge_dicts(rf_avg_abs_dicos)
+    #print(avg_abs_dico)
+    avg_rel_dico = merge_dicts(rf_avg_rel_dicos)
     # save method average distance
     metrics.save_dico(datadir, avg_abs_dico, "rf_distance_avg-abs")
     metrics.save_dico(datadir, avg_rel_dico, "rf_distance_avg-rel")
