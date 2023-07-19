a2a5ec87c9724afe1f2bf2baf4ba7d4fae2df418
knirschl <ucubm@student.kit.edu>
2023-07-20 01:17:34 +0200

    Structured pipeline.py into methods

--- src/py/knirschl/pipeline.py
+++ src/py/knirschl/pipeline.py
@@ -117,37 +117,7 @@ class RunFilter():
             except Exception as exc:
                 utils.printFlush("Failed running compare\n" + str(exc))
 
-
-# TOGGLE PIPELINE ELEMENTS
-# ====== ! CAREFUL ! ======
-run_filter = RunFilter() # all enabled
-#run_filter.force_overwrite = True # regenerate old dataset
-run_filter.bacomp() # only ba script
-# run_filter.comp() # only compare inferred trees
-# run_filter.disable_all() # collect avgs
-# ====== ! CAREFUL ! ======
-
-root_output = paths.families_datasets_root # output/families/
-# seeds 
-seeds5 = [42, 1007, 19732311, 121873, 14976684177860080345]
-seeds50 = [45364, 254864, 422868, 710192, 1142238, 1158688, 1424996, 1472190, 1513197, 1650734, 1716696, 1994998, 2190362, 2289044, 2436736, 2615935, 2620431, 2661376, 2722114, 2908180, 3044371, 3245068, 4040147, 4156463, 4257503, 4683309, 5087218, 5198455, 5355281, 5536663, 5543415, 5755677, 5942622, 6089587, 6329094, 6783683, 6947993, 7132734, 7308089, 7406226, 7781467, 8643158, 8726470, 8771421, 8846466, 9133069, 9376940, 9387366, 9481423, 9724682]
-seeds100 = [42, 1007, 4229, 11872, 121873, 125546, 340025, 429510, 436633, 491587, 558959, 842036, 901464, 1058026, 1091512, 1105070, 1143740, 1316419, 1320646, 1570525, 1674005, 2181913, 2366262, 2453027, 2525498, 2530855, 2545197, 2622038, 2650353, 2835503, 2862791, 2967039, 2967641, 3174338, 3219882, 3279056, 3389159, 3547108, 3614207, 3686221, 3939144, 3959336, 4108907, 4130562, 4144379, 4193573, 4290505, 4354376, 4412646, 4602564, 4754615, 4771372, 4785291, 4824437, 5476033, 5597160, 5616680, 5749959, 6050422, 6077656, 6214982, 6382595, 6449414, 6645706, 6722758, 6734655, 7060210, 7265233, 7340707, 7342556, 7350931, 7538361, 7724757, 7927143, 7940684, 8039292, 8124350, 8386866, 8388955, 8438226, 8527944, 8563774, 8670475, 8710215, 8928866, 9070912, 9128753, 9179296, 9387015, 9442585, 9476398, 9488374, 9495297, 9510809, 9718279, 9919929, 9930269, 9955696, 19732311, 14976684177860080345]
-seeds = seeds50
-#while (len(seeds) != 50):
-#    seeds.append(random.randrange(0, 9999999))
-# simphy params
-tag = "DL"
-d = l = 1.0
-s = 100 # def = 20
-replicates = []
-
-# Run multiple replicates
-for seed in seeds:
-    # SET simphy PARAMETERS 
-    simphy_parameters = simphy.SimphyParameters(tag=tag, seed=seed, dup_rate=d, loss_rate=l, species_taxa=s)
-    datadir = simphy.get_output_dir(simphy_parameters, root_output)
-    replicates.append(datadir)
-
+def pipeline(datadir, run_filter, seed, tag):
     # RUN PIPELINE
     rep_start = time.time()
     try:
@@ -157,43 +127,84 @@ for seed in seeds:
         print("End of single experiment. Elapsed time: " + str(elapsed) + "s")
         metrics.save_metrics(datadir, "pipeline_" + tag + str(seed), elapsed, "runtimes")
 
-# AVERAGE OVER ALL REPLICATES
-abs_name = "rf_distance_avg-abs"
-rel_name = "rf_distance_avg-rel"
-rt_name = "runtimes"
-abs_avgs_dico = metrics.get_metrics(replicates[0], abs_name)
-rel_avgs_dico = metrics.get_metrics(replicates[0], rel_name)
-rt_avgs_dico = metrics.get_metrics(replicates[0], rt_name)
-rep_counter = 1
-for rep in replicates[1:]:
-    cur_abs = metrics.get_metrics(rep, abs_name)
-    cur_rel = metrics.get_metrics(rep, rel_name)
-    cur_rt = metrics.get_metrics(rep, rt_name)
-#    abs_avgs_dico = {x: (float(abs_avgs_dico[x]) * rep_counter + float(cur_abs[x])) / (rep_counter + 1) for x in set(abs_avgs_dico).union(cur_abs)}
-    #    rel_avgs_dico = {x: (float(rel_avgs_dico[x]) * rep_counter + float(cur_rel[x])) / (rep_counter + 1) for x in set(rel_avgs_dico).union(cur_rel)}
-    for x in set(abs_avgs_dico).union(cur_abs):
-        if not x in abs_avgs_dico:
-            abs_avgs_dico[x] = 0
-            rel_avgs_dico[x] = 0
-        if not x in cur_abs:
-            cur_abs[x] = 0
-            cur_rel[x] = 0
-        abs_avgs_dico[x] = (float(abs_avgs_dico[x]) * rep_counter + float(cur_abs[x])) / (
-                    rep_counter + 1)
-        rel_avgs_dico[x] = (float(rel_avgs_dico[x]) * rep_counter + float(cur_rel[x])) / (
-                    rep_counter + 1)
-    for x in set(rt_avgs_dico).union(cur_rt):
-        if not x in rt_avgs_dico:
-            rt_avgs_dico[x] = 0
-        if not x in cur_rt:
-            cur_rt[x] = 0
-        rt_avgs_dico[x] = (float(rt_avgs_dico[x]) * rep_counter + float(cur_rt[x])) / (
-                    rep_counter + 1)
-    rep_counter += 1
+def run_pipeline(enabled = True):
+    # TOGGLE PIPELINE ELEMENTS
+    # ====== ! CAREFUL ! ======
+    run_filter = RunFilter() # all enabled
+    #run_filter.force_overwrite = True # regenerate old dataset
+    #run_filter.raxml = False
+    #run_filter.generax = False
+    #run_filter.force_overwrite = False
+    #run_filter.bacomp() # only ba script
+    run_filter.comp() # only compare inferred trees
+    # run_filter.disable_all() # collect avgs
+    # ====== ! CAREFUL ! ======
 
-rt_avgs_dico = {k: v for k, v in rt_avgs_dico.items() if not 'pipeline' in k}
-metrics.save_dico(root_output, abs_avgs_dico, tag + "_global__rf_distance_avg-abs")
-metrics.save_dico(root_output, rel_avgs_dico, tag + "_global__rf_distance_avg-rel")
-metrics.save_dico(root_output, rt_avgs_dico, tag + "_global__runtimes_avg")
+    root_output = paths.families_datasets_root # output/families/
+    # seeds 
+    seeds5 = [42, 1007, 19732311, 121873, 14976684177860080345]
+    seeds50 = [45364, 254864, 422868, 710192, 1142238, 1158688, 1424996, 1472190, 1513197, 1650734, 1716696, 1994998, 2190362, 2289044, 2436736, 2615935, 2620431, 2661376, 2722114, 2908180, 3044371, 3245068, 4040147, 4156463, 4257503, 4683309, 5087218, 5198455, 5355281, 5536663, 5543415, 5755677, 5942622, 6089587, 6329094, 6783683, 6947993, 7132734, 7308089, 7406226, 7781467, 8643158, 8726470, 8771421, 8846466, 9133069, 9376940, 9387366, 9481423, 9724682]
+    seeds100 = [42, 1007, 4229, 11872, 121873, 125546, 340025, 429510, 436633, 491587, 558959, 842036, 901464, 1058026, 1091512, 1105070, 1143740, 1316419, 1320646, 1570525, 1674005, 2181913, 2366262, 2453027, 2525498, 2530855, 2545197, 2622038, 2650353, 2835503, 2862791, 2967039, 2967641, 3174338, 3219882, 3279056, 3389159, 3547108, 3614207, 3686221, 3939144, 3959336, 4108907, 4130562, 4144379, 4193573, 4290505, 4354376, 4412646, 4602564, 4754615, 4771372, 4785291, 4824437, 5476033, 5597160, 5616680, 5749959, 6050422, 6077656, 6214982, 6382595, 6449414, 6645706, 6722758, 6734655, 7060210, 7265233, 7340707, 7342556, 7350931, 7538361, 7724757, 7927143, 7940684, 8039292, 8124350, 8386866, 8388955, 8438226, 8527944, 8563774, 8670475, 8710215, 8928866, 9070912, 9128753, 9179296, 9387015, 9442585, 9476398, 9488374, 9495297, 9510809, 9718279, 9919929, 9930269, 9955696, 19732311, 14976684177860080345]
+    seeds = seeds50
+    #while (len(seeds) != 50):
+    #    seeds.append(random.randrange(0, 9999999))
+    # simphy params
+    tag = "DL"
+    d = l = 1.0
+    s = 100 # def = 20
+    replicates = []
 
-print("seeds = ", seeds)
+    # Run multiple replicates
+    for seed in seeds:
+        # SET simphy PARAMETERS 
+        simphy_parameters = simphy.SimphyParameters(tag=tag, seed=seed, dup_rate=d, loss_rate=l, species_taxa=s)
+        datadir = simphy.get_output_dir(simphy_parameters, root_output)
+        replicates.append(datadir)
+        if enabled:
+            pipeline(datadir, run_filter, seed, tag)
+    return root_output, seeds, tag, replicates
+
+def global_compare(root_output, replicates, tag):
+    # AVERAGE OVER ALL REPLICATES
+    abs_name = "rf_distance_avg-abs"
+    rel_name = "rf_distance_avg-rel"
+    rt_name = "runtimes"
+    abs_avgs_dico = metrics.get_metrics(replicates[0], abs_name)
+    rel_avgs_dico = metrics.get_metrics(replicates[0], rel_name)
+    rt_avgs_dico = metrics.get_metrics(replicates[0], rt_name)
+    rep_counter = 1
+    for rep in replicates[1:]:
+        cur_abs = metrics.get_metrics(rep, abs_name)
+        cur_rel = metrics.get_metrics(rep, rel_name)
+        cur_rt = metrics.get_metrics(rep, rt_name)
+    #    abs_avgs_dico = {x: (float(abs_avgs_dico[x]) * rep_counter + float(cur_abs[x])) / (rep_counter + 1) for x in set(abs_avgs_dico).union(cur_abs)}
+        #    rel_avgs_dico = {x: (float(rel_avgs_dico[x]) * rep_counter + float(cur_rel[x])) / (rep_counter + 1) for x in set(rel_avgs_dico).union(cur_rel)}
+        for x in set(abs_avgs_dico).union(cur_abs):
+            if not x in abs_avgs_dico:
+                abs_avgs_dico[x] = 0
+                rel_avgs_dico[x] = 0
+            if not x in cur_abs:
+                cur_abs[x] = 0
+                cur_rel[x] = 0
+            abs_avgs_dico[x] = (float(abs_avgs_dico[x]) * rep_counter + float(cur_abs[x])) / (
+                        rep_counter + 1)
+            rel_avgs_dico[x] = (float(rel_avgs_dico[x]) * rep_counter + float(cur_rel[x])) / (
+                        rep_counter + 1)
+        for x in set(rt_avgs_dico).union(cur_rt):
+            if not x in rt_avgs_dico:
+                rt_avgs_dico[x] = 0
+            if not x in cur_rt:
+                cur_rt[x] = 0
+            rt_avgs_dico[x] = (float(rt_avgs_dico[x]) * rep_counter + float(cur_rt[x])) / (
+                        rep_counter + 1)
+        rep_counter += 1
+
+    rt_avgs_dico = {k: v for k, v in rt_avgs_dico.items() if not 'pipeline' in k}
+    metrics.save_dico(root_output, abs_avgs_dico, tag + "_global__rf_distance_avg-abs")
+    metrics.save_dico(root_output, rel_avgs_dico, tag + "_global__rf_distance_avg-rel")
+    metrics.save_dico(root_output, rt_avgs_dico, tag + "_global__runtimes_avg")
+
+if (__name__ == "__main__"):
+    root_output, seeds, tag, reps = run_pipeline()
+    global_compare(root_output, reps, tag)
+    print("seeds = ", seeds)
