b7ca945eb226ac3155077c89b261b4a2ac20921f
knirschl <ucubm@student.kit.edu>
2023-07-30 21:09:29 +0200

    Prepare main for extended scalings

--- rm_ba_rf.sh
+++ rm_ba_rf.sh
@@ -1,3 +1,4 @@
 find . -type f -name ba.\* -exec rm {} \;
 find . -type f -name rf_\* -exec rm {} \;
-find . -name "*.generax_pick*" -exec rename -vn ".generax_pick" "" {} \;
+find . -name "*.generax_pick*" -exec rename ".generax_pick" "" {} \;
+find . -type f -regex '.*/gene_trees/ba\.exp.[0-9].*\.newick' -delete
