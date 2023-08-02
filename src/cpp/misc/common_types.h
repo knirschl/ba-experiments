db4026fddf8aa79cab98626acde0aff88f3e9371
knirschl <ucubm@student.kit.edu>
2023-08-02 14:25:42 +0200

    Remove mat_scaled_add

--- src/cpp/misc/common_types.h
+++ src/cpp/misc/common_types.h
@@ -17,34 +17,6 @@ using dist_t = double;
 using dist_vector_t = vector_t<dist_t>;
 using dist_matrix_t = std::vector<dist_vector_t>;
 
-/**
-  * Scales two matrices with independent scales and adds them together. Writes the result to a new
-  * matrix and returns that one. The type parameter of the new matrix is the resulting type of
-  * (T + S).
-  *
- * @tparam T
- * @tparam S
- * @param left
- * @param right
- * @param scale_l
- * @param scale_r
- * @return the sum of the two scaled matrices
- */
-template<typename T, typename S>
-matrix_t<decltype(std::declval<T>() + std::declval<S>())>
-mat_scaled_add(matrix_t<T> const &left, matrix_t<S> const &right,
-               T const &scale_l = 1.0, S const &scale_r = 1.0) {
-    matrix_t<decltype(std::declval<T>() + std::declval<S>())> result;
-    result.resize(left.size());
-    for (int i{}; i < left.size(); i++) {
-        result[i].resize(left[i].size());
-        for (int j{}; j < left[i].size(); j++) {
-            result[i][j] = scale_l * left[i][j] + scale_r * right[i][j];
-        }
-    }
-    return result;
-}
-
 std::string vec_to_string(const vector_t<double> &vec) {
     return "[" + accumulate(vec.begin() + 1, vec.end(),
                             std::to_string(vec[0]), [](const std::string &acc, double b) {
