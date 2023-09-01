21e6131f63ed6fc72e5c9e0cb87ae6afb6e5124e
knirschl <ucubm@student.kit.edu>
2023-09-01 19:38:30 +0200

    Add benchmark selection to launcher

--- src/py/knirschl/launcher.py
+++ src/py/knirschl/launcher.py
@@ -8,9 +8,10 @@ import utils
 debug = False
 reps = 100
 parts = 1 # 10 should work in every case
-run_filter = "full"  
-enable_pip = False
-enable_eval = parts == 1
+run_filter = "s"  
+enable_pip = True
+enable_eval = run_filter != 's' and parts == 1
+bm_set = 1 # 0: benchmarks, 1: benchmarks_ext, 2: both
 
 # mistake?
 if (debug):
@@ -28,12 +29,12 @@ if (parts != 1 and run_filter == "comp"):
             run_filter = "full"
             print("Set run_filter to 'full'")
 if (enable_pip and enable_eval):
-    print("INFO: Computing and evaluating at the same time. Change [n,p,e]?")
+    print("INFO: Computing and evaluating at the same time. Change [n,c,e]?")
     match input():
         case 'e':
             enable_pip = False
             print("Disabled pipeline")
-        case 'p':
+        case 'c':
             enable_eval = False
             print("Disabled evaluation")
 if (enable_eval and (parts != 1 or run_filter != "full")):
@@ -57,17 +58,28 @@ benchmarks = {"BASE" : [-1],
             "BRALEN" : [0.01, 0.1, 10.0, 100.0],
             "DUPLOS" : [0.0, 0.5, 2.0, 3.0]}
 benchmarks_ext = {"SPECIES" : [],
-            "FAM" : [10, 50, 250],
+            #"FAM" : [10, 50, 250],
             "SITES" : [],
             "BRALEN" : [],
             "DUPLOS" : [],
             "TRA" : [],
             "DUPLOSTRA" : [],
-            "POPS" : [50, 1000, 470000000]}
+            "POP" : [50, 1000, 470000000]}
 
+bm = {}
+if bm_set == 0:
+    bm = benchmarks
+elif bm_set == 1:
+    bm = benchmarks_ext
+elif bm_set == 2:
+    for key in set(benchmarks.keys()).union(set(benchmarks_ext.keys())):
+        bm[key] = benchmarks[key] if key in benchmarks else []
+        if (key in benchmarks_ext and (vs := [v for v in benchmarks_ext[key] if v not in bm[key]]) != []):
+            bm[key].append(vs)
 reps_per_part = int(reps / parts)
-for bmark in benchmarks:
-    for val in benchmarks[bmark]:
+
+for bmark in bm:
+    for val in benchmarks_ext[bmark]:
         for part in range(parts):
             command = []
             if (cluster == "normal"):
