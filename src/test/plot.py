c10068108c2409f9e0558cd5649eb33555016387
knirschl <ucubm@student.kit.edu>
2023-09-04 01:30:33 +0200

    Cleanup plot.py

--- src/test/plot.py
+++ src/test/plot.py
@@ -35,20 +35,55 @@ BA_VARIANTS = [build_ba_variant(meth, a) for a in [APRO, MAD, ALL] for meth in [
     } """
 
 ## HELPERS
-def add_to_scatter(ax, vals, key, xlabel=None, ylabel=None, title=None):
+def rrf2xy(vals, key):
     x = []
     y = []
     for stat in vals[key]:
         x.append(stat[2][0])
         y.append(stat[1])
-    ax.scatter(x, y, label=key)#, c=COLORS[key])
-    ax.set_title(title)
-    ax.set(xlabel=xlabel, ylabel=ylabel)
+    return (x, y)
+
+def pick2xy(vals, key):
+    x = []
+    y = []
+    for sc in vals[key]:
+        #if (float(sc) <= 10):
+        x.append(float(sc))
+        y.append(int(vals[key][sc][0]))
+    return (x, y)
+
+def distdistr2xy(vals):
+    return ([i / 100 for i in range(len(vals))], vals)
+
+def get_filename(tags, save):
+    if not save:
+        return None
+    return os.path.join("/home/fili/Desktop/2023/BA/code/output/benchmark_results/figures", '_'.join(tags) + ".pdf")
+
+def add2plot(plot, xy, ptype="scatter", labels=5 * [None], zoom=''):
+    if (ptype == "scatter"):
+        plot.scatter(xy[0], xy[1], label=labels[0])#, c=COLORS[labels[0]])
+    elif (ptype == "bar"):
+        plot.bar(xy[0], xy[1], width=labels[4])#, color=COLORS[labels[0]]
+    else:
+        plot.plot(xy[0], xy[1], label=labels[0])#, color=COLORS[labels[0]]
+    plot.set(title=labels[1], xlabel=labels[2], ylabel=labels[3])
+    # TODO better zooming
+    if (zoom == "dyn"):
+        plot.set_xlim([min(xy[0]) - 2, max(xy[0]) + 2])
+        plot.set_ylim([min(xy[1]) - 0.01, max(xy[1]) + 0.01])
+    elif (zoom == "hard"):
+        plot.set_xlim([-0.2, 10.2])
+        plot.set_ylim([0, 1])
+    elif (zoom == "hardx"):
+        plot.set_xlim([-0.2, 10.2])
+    elif (zoom == "hardy"):
+        plot.set_ylim([0, 1])
 
 def display(figure, makegrid=True, makelegend=True, filename=None):
+    plt.grid(makegrid)
     if makelegend:
         plt.legend()
-    plt.grid(makegrid)
     if (filename):
         figure.savefig(filename, bbox_inches='tight')
     else:
@@ -60,8 +95,7 @@ def read_rrf(rrf_file):
     '''
     Returns a dict containing every tree that has an entry in the file.
     Keys = {'generax', 'raxml', 'fastme', 'ba' + algo, 'ba+fastme' + algo (, 'other')}
-    Values = [shortened tree name, rrf distance, [scale]
-    Values for ba-trees = {}
+    Values = [shortened tree name, rrf distance, [scale (, fastme matrix model, starting tree)]
     '''
     trees = {
         GENERAX: [],
@@ -102,7 +136,10 @@ def read_rrf(rrf_file):
 
 def read_picks(picks_file):
     '''
-    Returns ...
+    Returns a dict containing dicts per used scale.
+    Keys = {'ba' + algo, 'ba+fastme' + algo}
+    Values = {scaling values}
+    Sec. Valuies = [no. occurences, average distance, [dists], [trees]]
     '''
     trees = {}
     for var in BA_VARIANTS:
@@ -153,45 +190,44 @@ def read_distr(distr_file):
     
 # PLOTTER
 def plot_rrf(rrf_file, tag, save):
-    vals = read_rrf(rrf_file)
-    # plot all
-    fig, ax = plt.subplots()
-    for key in vals:
-        add_to_scatter(ax, vals, key, "Skalierungswerte", "rRF-Distanz", tag)
-    display(fig, filename=os.path.join("/home/fili/Desktop/2023/BA/code/output/benchmark_results/figures", tag + "_all_rf_distance-rel.pdf") if save else None)
-    # plot single ba(+fm) variants
-    for var in BA_VARIANTS:
-        if (var in vals):
-            fig, ax = plt.subplots()
-            add_to_scatter(ax, vals, var, "Skalierungswerte", "rRF-Distanz", var + " (" + tag + ")")
-            display(fig, makelegend=False, filename=os.path.join("/home/fili/Desktop/2023/BA/code/output/benchmark_results/figures", tag + "_" + var.lower() + "_rf_distance-rel.pdf") if save else None)
+    id = "rrf-dist"
+    labels = ['', tag, "Skalierungswerte", "rRF-Distanz"]
+    for zoom in ["auto", "hard"]:
+        vals = read_rrf(rrf_file)
+        # plot all
+        fig, ax = plt.subplots()
+        for key in vals:
+            labels[0] = key
+            add2plot(ax, rrf2xy(vals, key), labels=labels, zoom=zoom)
+        display(fig, filename=get_filename([tag, "all", zoom, id], save))
+        # plot single ba(+fm) variants
+        for var in BA_VARIANTS:
+            if (var in vals):
+                fig, ax = plt.subplots()
+                labels[0:2] = [var, var + " (" + tag + ")"]
+                add2plot(ax, rrf2xy(vals, var), labels=labels, zoom=zoom)
+                display(fig, makelegend=False, filename=get_filename([tag, var.lower(), zoom, id], save))
 
 def plot_picks(picks_file, tag, save):
+    labels = ['', "GeneRax-Picks (" + tag + ")", None, None, 0.05]
     vals = read_picks(picks_file)
     fig, ax = plt.subplots()
     for key in vals:
-        x = []
-        y = []
-        for sc in vals[key]:
-            if (float(sc) <= 10):
-                x.append(float(sc))
-                y.append(int(vals[key][sc][0]))
-        # TODO stack
-        ax.bar(x, y, label=key, width=0.05)#, c=COLORS[key])
-        ax.set_title("GeneRax-Picks (" + tag + ")")
-    display(fig, filename=os.path.join("/home/fili/Desktop/2023/BA/code/output/benchmark_results/figures", tag + "_generax_picks.pdf") if save else None)
+        labels[0] = key
+        # TODO stack, zoom
+        add2plot(ax, pick2xy(vals, key), ptype="bar", labels=labels)
+    display(fig, filename=get_filename([tag, "generax-picks"], save))
         
 
 def plot_distr(distr_file, tag, save):
+    labels = [None, "Distanzen der von GeneRax ausgewählten Bäume (" + tag + ")", "rRF Distanz", "Anzahl an Bäumen", 0.01]
     vals = read_distr(distr_file)
     # plot distances
     fig, ax = plt.subplots()
     ds = vals["distances"]
-    ax.bar([i / 100 for i in range(len(ds[0]))], ds[0], width=0.01)
+    add2plot(ax, distdistr2xy(ds[0]), ptype="bar", labels=labels)
     ax.axvline(ds[1] / 100, color='tab:green', label="avg")
-    ax.set_title("Distanzen der von GeneRax ausgewählten Bäume (" + tag + ")")
-    ax.set(xlabel="rRF Distanz", ylabel="Anzahl an Bäumen")
-    display(fig, makegrid=False, filename=os.path.join("/home/fili/Desktop/2023/BA/code/output/benchmark_results/figures", tag + "_distributions.pdf") if save else None)
+    display(fig, makegrid=False, filename=get_filename([tag, "dist-distr"], save))
 
 # MAIN
 def plot_matching_in_dir(dir, save=False):
@@ -209,6 +245,7 @@ def plot_matching_in_dir(dir, save=False):
             plot_distr(os.path.join(dir, f), tag, save)
     # TODO plots (dist, runtimes, ...) of full benchmarks (e.g. [BASE, DUPLOS0.0, DUPLOS0.5, DUPLOS2.0, DUPLOS3.0])
     # TODO collect them beforehand somehow ?
+    # TODO plots with error bars and not scatter
 
 if (__name__ == "__main__"):
     plot_matching_in_dir("/home/fili/Desktop/2023/BA/code/output/benchmark_results/metrics", save=False)
