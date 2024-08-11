c86697962afd17b621e8439f6d7466c0b6da187c
knirschl <lumokni@gmail.com>
2024-08-11 16:22:03 +0200

    Fix spearfish paths

--- src/py/knirschl/scripts/paths.py
+++ src/py/knirschl/scripts/paths.py
@@ -51,7 +51,8 @@ generax_exec = os.path.join(programs_root, "GeneRax", "build", "bin", "generax")
 # FastME
 fastme_exec = os.path.join(programs_root, "FastME", "bin",  "fastme")
 # Spearfish
-ba_exec = os.path.join(root, "Spearfish", "build", "spearfish")
+spearfish_exec = os.path.join(root, "Spearfish", "build", "spearfish")
+spearfish_wrapper_py = os.path.join(root, "Spearfish", "src", "py", "wrapper.py")
 # MPI
 mpischeduler_exec = os.path.join(programs_root, "MPIScheduler", "bin", "mpi-scheduler")
 # constants
